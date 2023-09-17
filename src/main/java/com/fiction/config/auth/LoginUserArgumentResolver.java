b319c5abe9e198bf94b0fca43370dfdce8ac25a7
robbietaek <robbietaek@gmail.com>
2023-09-17 18:14:09 +0900

    생성자 자동생성 어노테이션 추가, 클래스파일 스프링 컴포넌트에 추가

--- src/main/java/com/fiction/config/auth/LoginUserArgumentResolver.java
+++ src/main/java/com/fiction/config/auth/LoginUserArgumentResolver.java
@@ -2,12 +2,16 @@ package com.fiction.config.auth;
 
 import com.fiction.config.auth.dto.SessionUser;
 import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+@RequiredArgsConstructor
+@Component
 public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
 
   private final HttpSession httpSession;
